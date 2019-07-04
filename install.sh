c375e3f17847fde042089db7f83829630df442ef
BLACK HAT CYBER <51839125+BHC-FAKE@users.noreply.github.com>
2019-07-04 08:38:51 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,20 @@
+clear
+figlet -f slant "W A I T"|lolcat
+apt update && apt upgrade
+pkg install python2 -y
+pkg install git -y
+pkg install ruby -y
+pkg install openssh -y
+pkg install php -y
+pkg install curl -y
+pkg install mc -y
+pip install requests
+pip install mechanize
+gem install lolcat
+sleep 3
+figlet -f slant "SELESAI INSTALL"|lolcat
+git clone https://github.com/BHC-FAKE/HackCam
+cd HackCam
+clear
+bash HackCam.sh
+
